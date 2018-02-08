edff9bca30cb908997d36fca3b7748d9ccf3874c
nilutz <nicolutz@macca.local>
2018-02-08 12:57:51 +0100

    Pretty

--- gatsby-node.js
+++ gatsby-node.js
@@ -77,8 +77,7 @@ exports.onPostBuild = () => {
   console.log('Copying locales');
   fs.copySync(
     path.join(__dirname, '/src/locales'),
-    path.join(__dirname, '/public/locales'),
-  );
+    path.join(__dirname, '/public/locales'));
 };
 
 exports.modifyWebpackConfig = ({ config, stage }) => {
