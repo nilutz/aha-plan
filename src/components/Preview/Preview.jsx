c6e422ffb8bfd90d651afbf8951cf5aa38c3030f
nilutz <lutznico@aol.com>
2019-01-02 12:15:25 +0100

    max-width fallback

--- src/components/Preview/Preview.jsx
+++ src/components/Preview/Preview.jsx
@@ -43,13 +43,14 @@ const Preview = props => {
         <div className="md-cell md-cell--4">
           <Link to={post.frontmatter.path}>
             {isCover ? (
-              <img src={post.frontmatter.image} alt="" />
+              <img src={post.frontmatter.image} alt="" style="max-height: 300px; max-width: 250px;" />
             ) : (
               <Img
                 sizes={coverElem.sizes}
                 src={coverElem.sizes.src}
                 srcSet={coverElem.sizes.srcSet}
                 alt=""
+                style="max-height: 300px; max-width: 250px;" 
               />
             )}
           </Link>
