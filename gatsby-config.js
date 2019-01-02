54c2419a963e36af8affa05289098a32be679a45
nilutz <lutznico@aol.com>
2019-01-02 13:59:59 +0100

    git-gateway & identity

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,6 +33,7 @@ module.exports = {
       },
     },
     'gatsby-plugin-netlify-cms',
+    'gatsby-plugin-netlify-identity-widget',
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-styled-components',
     'gatsby-plugin-sass',
